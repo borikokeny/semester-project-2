073430aef41ff47d14ae84a53825413fd8f93504
Bori Kokeny <bori@minerali.hu>
2023-11-20 18:32:03 +0100

    adds list options

--- src/js/api/listing/list.js
+++ src/js/api/listing/list.js
@@ -1,1 +1,18 @@
-export function listListing(options) {}
+import { NAMESPACE } from "../../constants.js";
+
+export function listListing(options = {
+  sort: "created",
+  sortOrder: "desc",
+  limit: 10,
+  offset: 0,
+  _tag: NAMESPACE,
+  _active: true,
+  _seller: true,
+  _bids: true
+}, user) {
+  if (user) {
+    // Use profile/name/posts endpoint
+  } else {
+    // Use listings endpoints
+  }
+}
